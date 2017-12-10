9ea407c9ee8cf196370b72db80812aa2558e55d1
LI1994 <Lyndee_irish@hotmail.com>
2017-12-09 18:39:04 -0700

    server side done

---
+++ server/models/Villain.js
@@ -0,0 +1,11 @@
+const mongoose =  require('mongoose')
+const Schema = mongoose.Schema
+
+const Villain = new Schema({
+    name: {type: String, required: true},
+    img: {type: String, reqiured: true},
+    nemesis: String,
+    universe: String
+})
+
+module.exports=mongoose.model('Villain', Villain)
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 const SuperHero = require('./models/SuperHero')
+const Villain = require('./models/Villain')
 
 const port = 3001
 app.set('trust proxy', '127.0.0.1')
@@ -23,7 +24,7 @@ app.post('/api/heroes', (req, res) => {
    if (err) {
      res.json({ error: err })
    } else{
-     res.json({ msg: 'Your post was successfully created!😀', data: savedPost})
+     res.json({ msg: 'Your Hero was successfully created!🌟', data: savedPost})
    }
  })
 })
@@ -59,6 +60,44 @@ app.delete('/api/heroes/:heroId', (req, res) => {
     }
   })
 })
+
+const mySecondPost = {name:'Bob', img:'Url.Bob', nemesis:'Fred',universe:'Pluto'}
+
+app.post('/api/villain', (req, res) => {
+  const {name, img, nemesis, universe} = req.body
+  const newVillain = {name, img, nemesis, universe}
+
+  Villain (newVillain).save((err, savedPost) => {
+    if(err) {
+      res.json({ error: err })
+    } else {
+      res.json({msg: 'Your Villan was successfully created!⚡️', data: savedPost})
+    }
+  })
+})
+
+app.get('/api/villain', (req, res) => {
+  Villain.find((err, villains) => {
+    if (err) {
+      res.json({ error: err })
+    } else {
+      res.json({ msg: 'SUCCESS', villains})
+    }
+  })
+})
+
+app.delete('/api/villain/:villainId', (req, res) => {
+ const deleteId = req.params.villainId
+ Villain.remove({_id: deleteId}, (err, villain) => {
+   if (err) {
+    res.json({ error: err})
+   } else {
+     res.json({ msg: 'Your villain was deleted', villain})
+   }
+ })
+})
+
+
 
 const server = app.listen(port, () => console.log(`🔥Running on port: ${port}`))
 
